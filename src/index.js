496ced0f67a01999a929b5cf2e05bb64ac1a23d8
Sho Hashimoto <hashimoto@shokai.org>
2016-10-10 18:25:57 +0900

    show help banner

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import minimist from 'minimist'
 import Uploader from './uploader'
 import {wait} from './util'
+import pkg from '../package.json'
 
-export function start (argv) {
-  argv = minimist(argv)
-  const dir = argv['dir'] || argv['d'] || process.env.HOME
+export function start (_argv) {
+  const argv = minimist(_argv, {
+    boolean: [ 'help' ],
+    string: [ 'dir' ],
+    alias: {
+      'help': 'h',
+      'dir': 'd'
+    }
+  });
+  if (argv.help) {
+    console.log(banner)
+  }
+  if (typeof argv.dir !== 'string' || !argv.dir) {
+    console.error('--dir is missing')
+    process.exit(1)
+  }
+
+  const dir = argv['dir']
   console.log('watch', dir)
   const accessToken = process.env.GYAZO_ACCESS_TOKEN
   const uploader = new Uploader({dir, accessToken})
@@ -16,3 +32,13 @@ export function start (argv) {
   })
   wait()
 }
+
+const banner = `${pkg.name} v${pkg.version} - ${pkg.homepage}
+
+Usage:
+  % ${pkg.name} --dir ~/Dropbox/
+
+Options:
+  --help   show help
+  --dir    set directory
+`
